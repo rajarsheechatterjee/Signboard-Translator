a5624d707938c9802feef02d8a9f32113820314b
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-03-19 11:39:09 +0530

    OCR with Tesseract & OpenCV

--- client/src/screens/Translation.js
+++ client/src/screens/Translation.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
 
+import firebase from '../services/firebaseConfig';
+
 const Translation = ({ route, navigation }) => {
-  const { imageName } = route.params;
-  return <Text>{imageName}</Text>;
+	const { imageName } = route.params;
+
+	const getImageFromFirebase = () => {
+		// const storage = firebase.storage();
+		// const pathReference = storage
+		// 	.ref(`images/0960bd00-4310-41af-984f-e6092869004f.jpg`)
+		// 	.getDownloadURL();
+
+		// const encodedUri = encodeURIComponent(pathReference);
+		fetch(`http://127.0.0.1:5000/`, {
+			method: 'GET',
+			headers: {
+				'access-control-allow-origin': '*',
+				'Content-type': 'application/json;',
+			},
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));
+	};
+
+	useEffect(() => {
+		getImageFromFirebase();
+	}, []);
+
+	return <Text>{imageName}</Text>;
 };
 
 export default Translation;
